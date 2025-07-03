e8960a777025ccfc7189b04e65a82b4de59ec08e
Junnu <junnusalovaara@gmail.com>
2025-07-03 12:27:13 +0300

    deployment config files added

--- src/test/user.test.ts
+++ src/test/user.test.ts
@@ -1,5 +1,14 @@
+import * as user from "../handlers/user";
+
 describe("user handler", () => {
-  it("should do something when something happens", () => {
-    expect(1).toBe(1);
+  it("should create a new user", async () => {
+    const req = { body: { username: "hello", password: "hi" } };
+    const res = {
+      json({ token }) {
+        expect(token).toBeTruthy();
+      },
+    };
+
+    await user.createNewUser(req, res, () => {});
   });
 });
